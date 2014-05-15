397d4706138c20ed55a8e1cb79330dc44acefe76
Lleïr Borràs Metje <lleir@llegeix.me>
2014-05-15 09:58:32 +0200

    Scope datepicker with table

--- lib/filter/date_range_field_builder.rb
+++ lib/filter/date_range_field_builder.rb
@@ -10,14 +10,14 @@ module Filter
         haml_tag :fieldset do
           haml_concat label(@@form_namespace, input_name, options[:title] || attribute_name.to_s.humanize, for: 'date')
           haml_tag :br
-          haml_tag :div, class: "form-group input-daterange ccm-datepicker" do
+          haml_tag :div, class: "form-group input-daterange table-datepicker" do
             value = options[:value]
             haml_concat text_field(@@form_namespace, "#{input_name}_gteq", {value: from_value, class: 'form-control'})
           end
           haml_tag :div, class: "form-group delimiter" do
             haml_tag :label, "-"
           end
-          haml_tag :div, class: "form-group input-daterange ccm-datepicker" do
+          haml_tag :div, class: "form-group input-daterange table-datepicker" do
             value = options[:value]
             haml_concat text_field(@@form_namespace, "#{input_name}_lteq", {value: to_value, class: 'form-control'})
           end
