323e95063c9df6e7fc3e2e1435174b9b8ad9a2b2
Marcello Barnaba <vjt@openssl.it>
2014-05-29 05:14:55 +0200

    [HACKATON] Render selection fields with few choices as radio buttons

--- lib/filter/selection_field_builder.rb
+++ lib/filter/selection_field_builder.rb
@@ -1,20 +1,53 @@
 module Filter
   class SelectionFieldBuilder < Filter::BaseFieldBuilder
     def render object_class, attribute_name, options = {}
-      value = options[:value]
-      collection = options[:collection].is_a?(Proc) ? options[:collection].call : options[:collection]
+      @value = options[:value]
+      @collection = options[:collection].is_a?(Proc) ? options[:collection].call : options[:collection]
       input_name = options[:field_name].present? ? options[:field_name] : attribute_name
-      
-      field_name = if options[:attribute_type] == :ransack
+
+      @title = options.fetch(:title, attribute_name.to_s.humanize)
+      @field_name = if options[:attribute_type] == :ransack
         input_name
       else
         "#{input_name}_eq"
       end
 
-      haml_tag :div, class: "form-group" do
-        haml_concat label(@@form_namespace, field_name, options[:title] || attribute_name.to_s.humanize)
-        haml_concat select(@@form_namespace, field_name, collection, {selected: value, include_blank: 'Any'}, {class: 'form-control'})
+      haml_tag :div, class: 'form-group' do
+        haml_concat label(@@form_namespace, @field_name, @title)
+
+        (@collection.kind_of?(Array) && @collection.size <= 5) ? radios : dropdown
       end
     end
+
+
+    private
+      def dropdown
+        haml_concat select(@@form_namespace, @field_name, @collection, {selected: @value, include_blank: 'Any'}, {class: 'form-control'})
+      end
+
+      def radios
+        haml_tag :div, class: 'form-inline' do
+          any_checked = true
+
+          @collection.each do |title, value|
+            haml_tag :div, class: 'radio' do
+              checked = (@value.to_s == value.to_s)
+              any_checked = false if checked
+
+              haml_tag :label do
+                haml_concat radio_button(@@form_namespace, @field_name, value, checked: checked)
+                haml_concat title
+              end
+            end
+          end
+
+          haml_tag :div, class: 'radio' do
+            haml_tag :label do
+              haml_concat radio_button(@@form_namespace, @field_name, '', checked: any_checked)
+              haml_concat 'Any'
+            end
+          end
+        end
+      end
   end
 end
