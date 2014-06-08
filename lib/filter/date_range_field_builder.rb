58942eab91e173ee1c6a67f9f1efd2b7b0f97a19
Marcello Barnaba <vjt@openssl.it>
2014-06-08 23:59:46 +0200

    Remove excess wrapping

--- lib/filter/date_range_field_builder.rb
+++ lib/filter/date_range_field_builder.rb
@@ -6,13 +6,11 @@ module Filter
         to_value   = opts[:value].last
 
         haml_tag :div, class: "form-group" do
-          haml_tag :fieldset do
-            haml_concat label(opts[:namespace], attr, opts.fetch(:title, attr.to_s.humanize))
-            haml_tag :div, class: "input-daterange" do
-              haml_concat text_field(opts[:namespace], "#{attr}_gteq", {value: from_value, class: 'form-control'})
-              haml_tag :span, '-', class: "delimiter"
-              haml_concat text_field(opts[:namespace], "#{attr}_lteq", {value: to_value,   class: 'form-control'})
-            end
+          haml_concat label(opts[:namespace], attr, opts.fetch(:title, attr.to_s.humanize))
+          haml_tag :div, class: "input-daterange" do
+            haml_concat text_field(opts[:namespace], "#{attr}_gteq", {value: from_value, class: 'form-control'})
+            haml_tag :span, '-', class: "delimiter"
+            haml_concat text_field(opts[:namespace], "#{attr}_lteq", {value: to_value,   class: 'form-control'})
           end
         end
       end
