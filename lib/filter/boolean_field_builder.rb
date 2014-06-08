bbed74dc66557b545cb220d9fd830661b291b44d
Marcello Barnaba <vjt@openssl.it>
2014-06-08 16:21:49 +0200

    Remove leftover, give tri-state boolean option a self-descriptive name

--- lib/filter/boolean_field_builder.rb
+++ lib/filter/boolean_field_builder.rb
@@ -7,12 +7,12 @@ module Filter
         name = "#{attr}_eq"
 
         haml_tag :div, class: "form-group" do
-          if opts[:attribute_type] == :ransack
-            haml_concat label(opts[:namespace], attr, opts[:title] || attr.to_s.humanize)
-            haml_concat select(opts[:namespace], attr, [['Yes', 'true'], ['No', 'false']], {include_blank: 'Any', selected: value}, {class: 'form-control'})
-          elsif opts[:attribute_override_type] == :ransack
+          if opts.fetch(:tristate, false)
+
+            # TODO make this a radio
             haml_concat label(opts[:namespace], name, opts[:title] || attr.to_s.humanize)
             haml_concat select(opts[:namespace], name, [['Yes', 'true'], ['No', 'false']], {include_blank: 'Any', selected: value}, {class: 'form-control'})
+
           else
             haml_tag :div, class: 'checkbox' do
               haml_tag :label do
