c47a792700d27d3e93e04cc9d2bf6318da5dddcd
Marcello Barnaba <vjt@openssl.it>
2014-06-09 05:03:44 +0200

    Clean up

--- lib/filter/builder/search_field.rb
+++ lib/filter/builder/search_field.rb
@@ -6,14 +6,16 @@ module Filter
         attr  = opts[:is_scoped] ? attr : [attr, 'cont'].join('_')
 
         haml_tag :div, class: 'form-group' do
-          value = opts[:value]
+          input_options = { value: opts[:value], class: 'form-control', placeholder: opts[:title] || "Search #{attr.to_s.humanize}" }
+
           if opts[:skip_button]
             haml_concat label(opts[:namespace], attr, opts[:title] || "Search #{attr.to_s.humanize}")
-            haml_concat text_field(opts[:namespace], attr, value: value, class: 'form-control', placeholder: opts[:title] || "Search #{attr.to_s.humanize}")
+            haml_concat text_field(opts[:namespace], attr, input_options)
           else
             haml_tag :div, class: 'input-group' do
-              haml_concat text_field(opts[:namespace], attr, value: value, class: 'form-control', placeholder: opts[:title] || "Search #{attr.to_s.humanize}")
+              haml_concat text_field(opts[:namespace], attr, input_options)
               haml_tag :span, class: 'input-group-btn' do
+
                 haml_tag :button, type: 'button', class: 'btn btn-primary search' do
                   haml_tag :i, class: 'fa fa-search'
                   haml_concat 'Search'
