4b741ddc10b4ed76db5ee741fd5786a700d0b173
Marcello Barnaba <vjt@openssl.it>
2014-07-28 15:32:14 +0200

    Add ability to override the XLSX template name

--- lib/stradivari/xlsx/controller.rb
+++ lib/stradivari/xlsx/controller.rb
@@ -3,15 +3,15 @@ module Stradivari
 
     module Controller
       def render_xlsx(options = {})
-        xlsx = render_to_string
+        filename = options.delete(:filename) || 'export'
+        filename << '.xlsx' unless filename =~ /\.xlsx$/
+
+        xlsx = render_to_string(options)
 
         if xlsx[-1] == "\n" # HACK FIXME bypass HAML
           xlsx.slice! -1
           xlsx.concat "\x00".force_encoding('BINARY')*4
         end
-
-        filename = options.fetch(:filename, nil)
-        filename << '.xlsx' unless filename =~ /\.xlsx$/
 
         send_data xlsx, type: :xlsx,
           disposition: options.fetch(:disposition, 'attachment'),
