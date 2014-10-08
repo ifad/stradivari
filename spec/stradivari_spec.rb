23b5ae050ae45e43187e7859f5dcaedd04df2b48
stefano@interact.it <Jeko@JekBook-Air.local>
2014-10-08 14:28:21 +0200

    added some contexts to the spec

--- spec/stradivari_spec.rb
+++ spec/stradivari_spec.rb
@@ -1,8 +1,7 @@
 p "testing on Rails " << Rails::VERSION::STRING
 
 RSpec.describe "stradivari" do
-  context 'test context' do
-
+  context 'index page with foos data' do
     it "renders index page in html format" do
       visit "/welcome/index"
       save_result("welcome_index.html", page.source)
@@ -14,19 +13,17 @@ RSpec.describe "stradivari" do
       save_result("welcome_index.csv", page.source)
       expect(page.source).to eq(load_result "welcome_index.csv"), "expected same source"
     end
+  end
 
+  context 'details page with foo data' do
     it "renders details in html" do
       visit "/welcome/details"
       save_result("details.html", page.source)
       expect(page.source).to eq(load_result "details.html"), "expected same source"
     end
+  end
 
-    it "renders tree in html" do
-      visit "/welcome/tree"
-      save_result("tree.html", page.source)
-      expect(page.source).to eq(load_result "tree.html"), "expected same source"
-    end
-
+  context 'post page with posts data' do
     it "renders posts page in csv format" do
       visit "/welcome/posts.csv"
       save_result("posts.csv", page.source)
@@ -38,8 +35,14 @@ RSpec.describe "stradivari" do
       save_result("posts.html", page.source)
       expect(page.source).to eq(load_result "posts.html"), "expected same source"
     end
-
   end
 
+  context 'tree with no data' do
+    it "renders tree in html" do
+      visit "/welcome/tree"
+      save_result("tree.html", page.source)
+      expect(page.source).to eq(load_result "tree.html"), "expected same source"
+    end
+  end
 end
 
