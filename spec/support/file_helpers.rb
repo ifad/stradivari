68f298536674adefb9a8252825204434bff48641
stefano@interact.it <Jeko@JekBook-Air.local>
2014-10-10 14:38:26 +0200

    now the load_results syntax is more compact

--- spec/support/file_helpers.rb
+++ spec/support/file_helpers.rb
@@ -1,9 +1,7 @@
 module StradiTest
   module FileHelpers
     def load_result(name)
-      @result = ''
-      File.new(File.dirname(__FILE__) + "/../expected_results/#{name}").each_line { |l| @result += l }
-      @result
+      Pathname.new(__FILE__).join('../../expected_results', name).read
     end
 
     def save_result(name, result)
