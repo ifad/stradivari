22f2f679e9d04945453cf3579f3b3041fe6fb0d3
stefano@interact.it <Jeko@JekBook-Air.local>
2014-10-10 14:30:38 +0200

    compact syntax for sequences in the foo factory

--- spec/factories/foo.rb
+++ spec/factories/foo.rb
@@ -1,15 +1,11 @@
 # This will guess the Foo class
 FactoryGirl.define do
   factory :foo, :class => Foo do
-    sequence :id do |n|
-    	n
-    end
+    sequence(:id){|n| n}
     name "name field value"
     text_field "text field value"
     string_field "string field value"
-    sequence :boolean_field do |n|
-    	n%2
-    end
+    sequence(:boolean_field){|n| n%2}
     created_at "Mon, 29 Sep 2014 12:07:45 +0000"
     updated_at "Mon, 29 Sep 2014 12:07:45 +0000"
   end
