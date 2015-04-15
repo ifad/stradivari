d0af2787846d14b429b7fbe0f3e5add599aa92c5
Joe Blackman <j.blackman@ifad.org>
2015-04-15 11:11:49 +0200

    Added styling options for xlsx

--- lib/stradivari/xlsx/generator.rb
+++ lib/stradivari/xlsx/generator.rb
@@ -4,6 +4,20 @@ module Stradivari
   module XLSX
     class Generator < CSV::Generator
 
+      XLSX_OPTIONS = {
+        heading_style: {
+          font_name:  'Verdana',
+          sz:         15,
+          bg_color:   'dddddd',
+          fg_color:   '000000',
+          border:     Axlsx::STYLE_THIN_BORDER,
+        },
+        body_style: {
+          font_name:  'Verdana',
+          sz:         10,
+        }
+      }
+
       class Column < CSV::Generator::Column
         def to_s(object)
           if @renderer.present?
@@ -20,10 +34,16 @@ module Stradivari
         xlsx.to_stream.read.html_safe.force_encoding('BINARY')
       end
 
+      def initialize view, rows, *pass, &definition
+        super
+        opts.reverse_merge! XLSX_OPTIONS
+      end
+
       protected
         def xlsx
           Axlsx::Package.new do |package|
             package.use_shared_strings = true
+            # apply column styling if set
             package.workbook.add_worksheet(name: opts.fetch(:sheet, nil)) do |sheet|
               render_data(sheet)
             end
@@ -44,9 +64,7 @@ module Stradivari
         end
 
         def render_header(sheet)
-          heading = sheet.styles.add_style sz: 15,
-            bg_color: 'dddddd', fg_color: '000000',
-            border: Axlsx::STYLE_THIN_BORDER, font_name: 'Verdana'
+          heading = sheet.styles.add_style opts[:heading_style]
 
           sheet.add_row @columns.map(&:title),
             types: [:string] * @columns.size,
@@ -54,7 +72,7 @@ module Stradivari
         end
 
         def render_body(sheet)
-          @body_style = sheet.styles.add_style font_name: 'Verdana', sz: 10
+          @body_style = sheet.styles.add_style opts[:body_style]
 
           @data.each do |object|
             sheet.add_row(*render_row(object))
@@ -64,6 +82,13 @@ module Stradivari
                 sheet.add_row(*render_row(child))
               end
             end
+          end
+
+          # Apply column styles
+          @columns.each_with_index do |col, idx|
+            style_def = col.opts[:style] or next
+            style = sheet.styles.add_style style_def
+            sheet.col_style idx, style, row_offset: 1 # applies style, skips heading
           end
         end
 
