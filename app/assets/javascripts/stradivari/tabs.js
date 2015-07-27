3e5d73891735d00c37a81edd6b54d327ec39f95c
Stefano Guglielmetti <stefano@mikamai.com>
2015-07-27 18:19:43 +0200

    second version of the automagic tabs opener

--- app/assets/javascripts/stradivari/tabs.js
+++ app/assets/javascripts/stradivari/tabs.js
@@ -32,11 +32,11 @@ jQuery(function() {
 
   // activate tab if tab id is specified in the url stradi_tabs[] parameter
   // clicking the tab link works even with ajax tabs
-  var stradivari_tabs = _TABLE_.parseURLParameters(location.href)["stradi_tabs[]"];
+  var stradivari_tabs = _TABLE_.parseURLParameters(location.href)["stradi_tabs"];
 
   if ( stradivari_tabs != undefined ) {
     $.each(stradivari_tabs, function(i, tab_id){
-      var tab = $("[data-toggle='tab'][href='" + tab_id + "']").first();
+      var tab = $("[data-toggle='tab'][href='#" + tab_id + "']").first();
       if (tab != undefined) tab.click();
     })
   }
