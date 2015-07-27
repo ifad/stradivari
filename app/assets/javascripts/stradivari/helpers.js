cbad9f4d9e613340a8dd42cf1d52a766601747eb
Marcello Barnaba <vjt@openssl.it>
2015-07-27 19:15:33 +0200

    Clean up helpers.js a bit
    * move URI decoding in a new shared function
    * always return an object, even when there are no query string params
    * use consistent whitespace

--- app/assets/javascripts/stradivari/helpers.js
+++ app/assets/javascripts/stradivari/helpers.js
@@ -17,6 +17,11 @@ window._TABLE_ = (function() {
       window.location.pathname
     );
   };
+  
+  var decodeURIComponentAndSpaces = function(str) {
+    str = str.replace(/\+/g, ' ');
+    return window.decodeURIComponent(str);
+  };
 
   // API
   return {
@@ -46,21 +51,23 @@ window._TABLE_ = (function() {
     // in: http://mysite.com?stradi_tabs[]=bar&stradi_tabs[]=baz&foo=bar
     // out: {stradi_tabs: ['bar', 'baz'], foo: "bar"}
     parseURLParameters: function(uri) {
-      var re = /([^&=]+)=?([^&]*)/g;
-      var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
-      var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );};
-      var query = uri.split('?')[1]
-      var params = {}, e;
+      var re     = /([^&=]+)=?([^&]*)/g;
+      var query  = uri.split('?')[1]
+      var params = {};
 
-      if (query == undefined) return false;
+      if (query) {
+        var e;
+        
+        while (e = re.exec(query)) {
+          var k = decodeURIComponentAndSpaces(e[1]),
+              v = decodeURIComponentAndSpaces(e[2]);
 
-      while ( e = re.exec(query) ) {
-          var k = decode( e[1] ), v = decode( e[2] );
           if (k.substring(k.length - 2) === '[]') {
-              k = k.substring(0, k.length - 2);
-              (params[k] || (params[k] = [])).push(v);
+            k = k.substring(0, k.length - 2);
+            (params[k] || (params[k] = [])).push(v);
           }
           else params[k] = v;
+        }
       }
       return params;
     }
