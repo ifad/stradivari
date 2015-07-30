7829c7243c93f3879f7e194dea78e3a5f6353101
Marcello Barnaba <vjt@openssl.it>
2015-07-30 14:45:38 +0200

    Show collapsed field controls when changing radios_count option

--- lib/stradivari/filter/builder/selection_field.rb
+++ lib/stradivari/filter/builder/selection_field.rb
@@ -10,13 +10,15 @@ module Stradivari
           title      = opts.fetch(:title, attr.to_s.humanize)
           attr       = opts[:is_scoped] ? attr : [attr, 'eq'].join('_')
 
-          opts[:collapsed_field] = true if opts[:value].present? & collection.kind_of?(Array) && collection.size <= 5
+          radios_max = opts.fetch(:radios_count, 5)
+
+          opts[:collapsed_field] = true if opts[:value].present? & collection.kind_of?(Array) && collection.size <= radios_max
 
           haml_tag :div, class: 'form-group' do
             instance_exec(&Helpers::render_title(attr, title, opts))
 
             haml_tag :div, class: Builder::prepare_classes(opts) do
-              if collection.kind_of?(Array) && collection.size <= opts.fetch(:radios_count, 5)
+              if collection.kind_of?(Array) && collection.size <= radios_max
                 instance_exec(&Helpers.radios_for_collection(collection, attr, opts))
               else
                 haml_concat select(opts[:namespace], attr, collection, {selected: opts[:value], include_blank: 'Any'}, {class: 'form-control'})
