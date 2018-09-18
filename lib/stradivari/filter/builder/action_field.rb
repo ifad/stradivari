a7a3008b19d0a00dfe73ab4f41c01928418546fa
Mirko Mignini <mirko.mignini@gmail.com>
2018-09-18 14:32:47 +0200

    Filter actions locale

--- lib/stradivari/filter/builder/action_field.rb
+++ lib/stradivari/filter/builder/action_field.rb
@@ -8,11 +8,11 @@ module Stradivari
             haml_tag :li, class: 'list-group-item' do
               haml_tag :button, class: 'btn btn-primary btn-sm search' do
                 haml_tag :i, '', class: 'fa fa-search'
-                haml_concat ' Search'
+                haml_concat I18n.t("stradivari.filter.search", default: 'Search')
               end
               haml_tag :button, class: 'btn btn-default btn-sm clear' do
                 haml_tag :i, '', class: 'fa fa-times'
-                haml_concat ' Clear'
+                haml_concat I18n.t("stradivari.filter.clear", default: 'Clear')
               end
             end
           end
